b00ac61a8351a7c59aec7802dbd12b1682cf8110
evgenij30 <you@example.com>
2018-03-28 21:00:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var courses = require('./data/courses.json');
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/publick'));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+
+app.get('/',function (req, res) {
+    res.render('index',{title:"API"});
+});
+app.get('/api/courses',function (req, res) {
+    res.render('courses',{
+        title:"courses",
+        courses: courses
+
+    });
+});
+app.listen(3000, function () {
+    console.log('Go to localhost 3000');
+})
